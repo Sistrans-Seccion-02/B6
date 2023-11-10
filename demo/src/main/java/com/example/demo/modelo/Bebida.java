f81f1118bc7943edc2134933e16763ceeae8e375
Vanessa Martinez <l.martinezp@uniandes.edu.co>
2023-11-10 15:24:26 -0500

    Primera parte done

--- demo/src/main/java/com/example/demo/modelo/Bebida.java
+++ demo/src/main/java/com/example/demo/modelo/Bebida.java
@@ -3,7 +3,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document
+@Document (collection = "bebidas")
 public class Bebida {
     @Id
     private String id;
